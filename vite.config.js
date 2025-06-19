09d15c786631ce9b533e055d05013ace332c302a
Binara-Prabhanga <binaraprabhanga@gmail.com>
2025-06-19 13:03:55 +0530

    ..

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,20 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-    base: '/build/', 
+    base: '/build/', // Keep this if you're serving from /public/build
+    server: {
+        https: true, // <- This line is optional for local dev, not needed in production
+    },
+    build: {
+        manifest: true,
+        outDir: 'public/build',
+        rollupOptions: {
+            input: 'resources/js/app.js',
+        },
+    },
     plugins: [
         laravel({
-            input: 'resources/js/app.js',
+            input: ['resources/js/app.js'],
             refresh: true,
         }),
         vue({
